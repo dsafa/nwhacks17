daeed25374cfca35fc725b6eab7eafe8c90138d7
brandon chong <brandon chong>
2017-03-18 21:01:07 -0700

    fix async

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 var app = require('express')()
 var multer = require('multer')()
 var pg = require('pg')
+var async = require('async')
 
 var config = {
   user: 'test',
@@ -20,33 +21,33 @@ pg.connect(config, function (err, client, done) {
     console.error('could not connect to cockroachdb', err);
     finish();
   }
-  async.waterfall([
-    function (next) {
-      // Create the "accounts" table.
-      client.query("CREATE TABLE IF NOT EXISTS accounts (id INT PRIMARY KEY, balance INT);", next);
-    },
-    function (next) {
-      // Insert two rows into the "accounts" table.
-      client.query("INSERT INTO accounts (id, balance) VALUES (1, 1000), (2, 250);", next);
-    },
-    function (results, next) {
-      // Print out the balances.
-      client.query('SELECT id, balance FROM accounts;', next);
-    },
-  ],
-  function (err, results) {
-    if (err) {
-      console.error('error inserting into and selecting from accounts', err);
-      finish();
-    }
+  // async.waterfall([
+  //   function (next) {
+  //     // Create the "accounts" table.
+  //     client.query("CREATE TABLE IF NOT EXISTS accounts (id INT PRIMARY KEY, balance INT);", next);
+  //   },
+  //   function (next) {
+  //     // Insert two rows into the "accounts" table.
+  //     client.query("INSERT INTO accounts (id, balance) VALUES (1, 1000), (2, 250);", next);
+  //   },
+  //   function (results, next) {
+  //     // Print out the balances.
+  //     client.query('SELECT id, balance FROM accounts;', next);
+  //   },
+  // ],
+  // function (err, results) {
+  //   if (err) {
+  //     console.error('error inserting into and selecting from accounts', err);
+  //     finish();
+  //   }
 
-    console.log('Initial balances:');
-    results.rows.forEach(function (row) {
-      console.log(row);
-    });
+  //   console.log('Initial balances:');
+  //   results.rows.forEach(function (row) {
+  //     console.log(row);
+  //   });
 
-    finish();
-  });
+  //   finish();
+  // });
 });
 
 
