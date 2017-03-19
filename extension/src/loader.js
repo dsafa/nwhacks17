2f80de7ff0c6e97c09543d4e538df4d4774b1886
Anthony Smith <anthony@smith.cm>
2017-03-18 23:48:10 -0700

    complete re-write

---
+++ extension/src/loader.js
@@ -0,0 +1,33 @@
+class Loader {
+    loadUrl() {
+        alert('hello');
+    }
+}
+
+$(document).ready(function() {
+
+    // Get the lat / long of the ad's location
+    var maps_box = document.getElementById("map");
+    var latitude = maps_box.dataset.latitude;
+    var longitude = maps_box.dataset.longitude;
+
+    // Find location to place our embedded element
+    var location_to_append = document.getElementsByClassName("mapbox");
+
+    // Create a simple button with text
+    var some_button = document.createElement("button");
+    var text = document.createTextNode("test");
+    some_button.appendChild(text);
+
+    // Place our embedded element onto the page
+    location_to_append[0].after(some_button);
+
+    const loader = new Loader();
+    loader.loadUrl();
+
+    //Inject inject.html into proper location
+    var iframe = document.createElement("iframe");
+    iframe.src = chrome.extension.getURL("injected/inject.html");
+
+});
+
