1bac1c88d3771b37f5678e1fc9829e1e13a867e2
Anthony Smith <anthony@smith.cm>
2017-03-19 07:14:26 -0700

    Add reload button to injection

--- extension/injected/loader.js
+++ extension/injected/loader.js
@@ -1,6 +1,12 @@
-const WEB_URL = "https://safesales.localtunnel.me/";
+const WEB_URL = "https://safesale.localtunnel.me/";
+const apiHelper = new APIHelper();
+const renderer = new Renderer();
 
-function RestAPI() {
+var isLoading = false;
+var seller_latitude = "";
+var seller_longitude = "";
+
+function APIHelper() {
     var self = this;
 
     this.sendPost = function(req_location, req_body, success_func, error_func) {
@@ -15,6 +21,38 @@ function RestAPI() {
         });
     }
 
+    this.loadData = function() {
+        navigator.geolocation.getCurrentPosition(function(position) {
+
+            var user_latitude = position.coords.latitude;
+            var user_longitude = position.coords.longitude;
+
+            // Send a request for data from remote server
+            var req_data = {
+                location1: user_latitude + "," + user_longitude,
+                location2: seller_latitude + "," + seller_longitude,
+                radius: 2,
+                size: 4,
+                mode: "driving"
+            };
+
+            self.sendPost(
+                    "api/location",
+                    req_data,
+                    function(data) {
+                        renderer.displayContent(data);
+                    },
+                    function(error) {
+                        console.log(error);
+                        var message = {
+                            "text": "An error occurred, please try again later."
+                        };
+                        renderer.displayError(message);
+                    }
+                );
+        });
+    }
+
 }
 
 function Renderer() {
@@ -24,20 +62,30 @@ function Renderer() {
         var template = Handlebars.compile(source);
         var html = (context != null) ? template(context) : template(context);
         $("#inject-display").html(html);
+
+        $("#mapreload").off('click').on('click', function(e) {
+            if(!isLoading) {
+                renderer.displayLoading();
+                apiHelper.loadData();
+            }
+        });
     }
 
     this.displayLoading = function() {
         var source = $("#loading").html();
+        isLoading = true;
         self.renderContent(source, {});
     }
 
     this.displayContent = function(data) {
         var source = $("#items").html();
+        isLoading = false;
         self.renderContent(source, data);
     }
 
     this.displayError = function(data) {
         var source = $("#error").html();
+        isLoading = false;
         self.renderContent(source, data);
     }
 
@@ -51,11 +99,8 @@ $(document).ready(function() {
 
     // Get the lat / long of the ad's location
     var maps_box = document.getElementById("map");
-    var seller_latitude = maps_box.dataset.latitude;
-    var seller_longitude = maps_box.dataset.longitude;
-
-    const restAPI = new RestAPI();
-    const renderer = new Renderer();
+    seller_latitude = maps_box.dataset.latitude;
+    seller_longitude = maps_box.dataset.longitude;
 
     // Load the HTML responsible for displaying the locations to the display
     var data = {};
@@ -63,37 +108,8 @@ $(document).ready(function() {
     $(".mapbox").append(data.bodyText);
     $("#injection").load(chrome.extension.getURL("injected/inject.html"), renderer.displayLoading);
 
-    // Get user location
-    navigator.geolocation.getCurrentPosition(function(position) {
-        console.log("Latitude: " + position.coords.latitude +
-        " Longitude: " + position.coords.longitude);
-
-        var user_latitude = position.coords.latitude;
-        var user_longitude = position.coords.longitude;
-
-        // Send a request for data from remote server
-        var req_data = {
-            location1: user_latitude + "," + user_longitude,
-            location2: seller_latitude + "," + seller_longitude,
-            radius: 2,
-            size: 4
-        };
-
-        restAPI.sendPost(
-                "api/location",
-                req_data,
-                function(data) {
-                    renderer.displayContent(data);
-                },
-                function(error) {
-                    console.log(error);
-                    var message = {
-                        "text": "An error occurred, please try again later."
-                    };
-                    renderer.displayError(message);
-                }
-            );
-    });
+    // Initial Load
+    apiHelper.loadData();
 
 });
 
