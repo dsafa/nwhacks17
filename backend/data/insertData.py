657aa66ee2083ee491c6b8fb21687aee9d1c5315
brandon chong <brandon chong>
2017-03-18 23:19:37 -0700

    Fix script

--- backend/data/insertData.py
+++ backend/data/insertData.py
@@ -1,9 +1,12 @@
-with open("script.sh", "w") as script:
-	script.write("#!/bin/sh\n")
-	script.write("cockroach sql --url=postgresql://root@54.186.70.223:26257?sslmode=disable\n")
-	script.write("DELETE TABLE Hotspot;\n")
+with open("scriptInput.txt", "w") as input:
+	input.write("cockroach sql --url=postgresql://root@54.186.70.223:26257?sslmode=disable\n")
+	input.write("DELETE FROM Hotspot;\n")
 
 	hotspots = [line.rstrip('\n') for line in open('hotspots.csv')]
 	for hotspot in hotspots:
 		insertStr = "INSERT INTO Hotspot (Neighbourhood, Lat, Long) VALUES ({});\n".format(hotspot)
-		script.write(insertStr)
+		input.write(insertStr)
+
+with open("script.sh", "w") as script:
+	script.write("#!/bin/sh\n")
+	script.write("/bin/sh < scriptInput.txt")
