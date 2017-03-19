e1eb444b31c7a387b8f44b0c56c776c3542bbc12
brandon chong <brandon chong>
2017-03-18 22:55:46 -0700

    add data

---
+++ backend/data/insertData.py
@@ -0,0 +1,9 @@
+with open("script.sh", "w") as script:
+	script.write("#!/bin/sh\n")
+	script.write("cockroach sql --url=postgresql://root@54.186.70.223:26257?sslmode=disable\n")
+	script.write("DELETE TABLE Hotspot;\n")
+
+	hotspots = [line.rstrip('\n') for line in open('hotspots.csv')]
+	for hotspot in hotspots:
+		insertStr = "INSERT INTO Hotspots (Neighbourhood, Lat, Long) VALUES ({});\n".format(hotspot)
+		script.write(insertStr)
