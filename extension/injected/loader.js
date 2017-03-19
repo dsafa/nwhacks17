1ec4b76fa9fc81aa02d4124a60b17df55e6cfd68
Steven Shih <stevenshih1997@gmail.com>
2017-03-19 00:07:05 -0700

    update

--- extension/injected/loader.js
+++ extension/injected/loader.js
@@ -1,8 +1,3 @@
-class Loader {
-    loadUrl() {
-        alert('hello');
-    }
-}
 
 $(document).ready(function() {
 
@@ -14,20 +9,11 @@ $(document).ready(function() {
     // Find location to place our embedded element
     var location_to_append = document.getElementsByClassName("mapbox");
 
-    // Create a simple button with text
-    var some_button = document.createElement("button");
-    var text = document.createTextNode("test");
-    some_button.appendChild(text);
-
-    // Place our embedded element onto the page
-    location_to_append[0].after(some_button);
-
-    const loader = new Loader();
-    loader.loadUrl();
 
     //Inject inject.html into proper location
     var iframe = document.createElement("iframe");
     iframe.src = chrome.extension.getURL("injected/inject.html");
+    location_to_append[0].after(iframe);
 
 });
 
